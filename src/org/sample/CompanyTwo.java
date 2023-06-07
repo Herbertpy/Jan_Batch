33644208d37ab85b416ae95f1bc6cb227faecbff
Herbertpy <herbetraj784@gmail.com>
2023-06-07 09:29:00 +0530

    Company Two Execution Added

---
+++ src/org/sample/CompanyTwo.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class CompanyTwo {
+	private void methodOne() {
+		System.out.println("TCS");
+	}
+
+	private void methodTwo() {
+		System.out.println("Wipro");
+	}
+	
+	private void methodThree() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodFour() {
+		// TODO Auto-generated method stub
+
+	}
+}
